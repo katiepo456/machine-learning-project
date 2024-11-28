94d8aa5643a8d3de54bbf06a156e14e518739952
katiepo456 <kathryn.james@mymail.champlain.edu>
2024-11-27 22:46:38 -0500

    working with libraries instead

--- linear_regression.py
+++ linear_regression.py
@@ -3,9 +3,12 @@
 
 import csv
 import copy
+import statistics
+import numpy as np
 from typing import Callable
 from math import ceil
 from pathlib import Path
+from sklearn import linear_model
 
 
 # Read a CSV file
@@ -25,11 +28,11 @@ def read_file(file_path: Path) -> tuple[list[list[float]], list[float]]:
         for row in reader:
             x_list = []
             ys.append(float(row[0]))      # salary
-            # x_list.append(float(row[1]))  # time frame for securing position
-            # x_list.append(float(row[2]))  # relatedness to career goals
+            x_list.append(float(row[1]))  # time frame for securing position
+            x_list.append(float(row[2]))  # relatedness to career goals
             x_list.append(float(row[3]))  # studied abroad?
             x_list.append(float(row[4]))  # held a student leadership position?
-            # x_list.append(float(row[5]))  # worked with center of experience?
+            x_list.append(float(row[5]))  # worked with center of experience?
             x_list.append(float(row[6]))  # number of internships
             x_list.append(float(row[7]))  # GPA
             xs.append(x_list)
@@ -165,4 +168,23 @@ def starter_run():
 
 
 if __name__ == '__main__':
-    starter_run()
+    p = Path(__file__).with_name('csi_alum_data.csv')
+    original_xs, original_ys = read_file(p.absolute())
+
+    reg = linear_model.LinearRegression(fit_intercept=True)
+    xs = original_xs
+    ys = original_ys
+    reg.fit(xs, ys)
+
+    print(f'------------------------------------------------------------------------- ALL DATA POINTS ----------------'
+          f'---------------------------------------------------------')
+    print(f'y = {round(reg.intercept_,2)} + {round(reg.coef_[0],2)}(time_frame) + '
+          f'{round(reg.coef_[1],2)}(relatedness) + {round(reg.coef_[2],2)}(studied_abroad) + '
+          f'{round(reg.coef_[3],2)}(student_leadership_position) + {round(reg.coef_[4],2)}(center_of_experience) + '
+          f'{round(reg.coef_[5],2)}(number_of_internships) + {round(reg.coef_[6],2)}(gpa)')
+    print(f'r-squared: {reg.score(xs, ys)}')
+
+    print(f'Outliers Removed from Data')
+    std_dev = np.std(original_xs, axis=0)
+
+    print(std_dev)
