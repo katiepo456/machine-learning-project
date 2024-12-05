d5d68f95b0d7b645226afcef69d6c0f7efdaa55d
Harrison.Blair <Harrison.blair@mymail.champlain.edu>
2024-12-05 15:08:27 -0500

    Made random forest functional, added auto parameter tuning

--- random_forest_regressor.py
+++ random_forest_regressor.py
@@ -1,31 +1,69 @@
+from sklearn.metrics import mean_squared_error
 import sklearn.tree
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn.ensemble
+from sklearn.model_selection import train_test_split
 
-df = pd.read_csv('csi_alum_data.csv')
 
-x = df.iloc[:, 1:8].values  # Features
-y = df.iloc[:, 0].values  # Target Variable
+def plot_model(model):
+    # Try to plot, if we cant, display data
+    try:
+        tree_to_plot = model.estimators_[0]
 
-regressor = sklearn.ensemble.RandomForestRegressor(max_leaf_nodes=20, random_state=0, oob_score=True)
+        plt.figure(figsize=(10,20))
+        sklearn.tree.plot_tree(tree_to_plot, feature_names=df.iloc[:,1:8].columns.tolist(), filled=True, rounded=True, fontsize=5)
+        plt.title("Decision Tree")
+        plt.show()
 
-regressor.fit(x, y)
+    except:
+        print("*" * 50)
+        print("Model does not have a tree to plot")
+        print(f"Optimal Params: {model.best_params_}")
+        print(f"Best Score: {model.best_score_}")
+        print(f"Features: {model.best_estimator_.feature_names_in_}")
+        print(f"Feature Importances: {model.best_estimator_.feature_importances_}")
 
-print(f"Out-of-bag score: {regressor.oob_score_}")
+def tune_parameters(model, X_train, y_train):
+    param_grid = {
+        'n_estimators': [200, 400, 600, 800],
+        'max_depth': [2, 4, 5, 7],
+        'max_leaf_nodes': [10, 13, 15, 17],
+        'max_features': [1, 3, 5, 7]
+    }
 
-predictions = regressor.predict(x)
+    rfr_cv = sklearn.model_selection.GridSearchCV(sklearn.ensemble.RandomForestRegressor(), param_grid, cv=4, n_jobs=-1)
 
-mse = sklearn.metrics.mean_squared_error(y, predictions)
-print(f"Mean squared error: {mse}")
+    rfr_cv.fit(X_train, y_train)
 
-r2 = sklearn.metrics.r2_score(y, predictions)
-print(f"R2 score: {r2}")
+    return rfr_cv
 
-tree_to_plot = regressor.estimators_[0]
+def test_and_graph(models, X_test, y_test):
+    for model in models:
+        print("=" * 50)
+        print(f"Model: {model.__class__.__name__}")
+        print(f"R Squared Score: {model.score(X_test, y_test)}")
+        print(f"MSE: {mean_squared_error(y_test, model.predict(X_test))}")
+        plot_model(model)
 
-plt.figure(figsize=(10,8))
-sklearn.tree.plot_tree(tree_to_plot, feature_names=df.iloc[:,1:8].columns.tolist(), filled=True, rounded=True, fontsize=5)
-plt.title("Decision Tree")
-plt.show()
+if __name__ == "__main__":
+    csi_df = pd.read_csv('csi_alum_data.csv')
+    cncs_df = pd.read_csv('cncs_alum_data.csv')
+    cdf_df = pd.read_csv('cdf_alum_data.csv')
+
+    df = pd.concat([csi_df, cncs_df, cdf_df])
+
+    #['salary', 'time_frame_for_securing_position', 'relatedness_to_career_goals', 'study_abroad?', 'student_leadership_position?', 'center_of_experience?', 'number_of_internships', 'GPA'
+
+    df = df.drop(['student_leadership_position?', 'center_of_experience?'], axis='columns')
+
+    model = sklearn.ensemble.RandomForestRegressor(max_depth=5, max_features=1, max_leaf_nodes=13, n_estimators=200)
+    
+    X_train, X_test, y_train, y_test = train_test_split(df.drop(['salary'], axis='columns'), df.salary, test_size=0.3)
+
+    model.fit(X_train, y_train)
+
+    rfr_tuned = tune_parameters(model, X_train, y_train)
+
+    test_and_graph([model, rfr_tuned], X_test, y_test)
